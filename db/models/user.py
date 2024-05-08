1f414c0051e3ae17219b7ef844f481bf68e157f0
MohammedRifadCyberSuare <rifad@cybersquare.com>
2024-05-08 13:39:38 +0530

    feat: workflow created

--- db/models/user.py
+++ db/models/user.py
@@ -119,3 +119,9 @@ class VerificationCode(models.Model):
     )
     code = models.CharField(max_length = 20, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+class TestDBWorkFLow(models.Model):
+     
+    code = models.CharField(max_length = 20, unique=True)
+    
+
