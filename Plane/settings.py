c1eb239cb6fe8ecb0d42b3759487c5140d80d279
fidha-na <fidha@cybersquare.org>
2024-06-07 11:38:52 +0530

    feat:templates-invitation-link

--- Plane/settings.py
+++ Plane/settings.py
@@ -214,8 +214,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'csproplane@gmail.com'
 EMAIL_HOST_PASSWORD = 'qkxxhhpdlwxzqkku'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
+
+
